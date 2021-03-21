4f4eed5b807427d3b15084b823d546151c1e3ceb
Kostiantyn Hantsov <k.gantsov@gmail.com>
2021-03-21 16:16:29 +0100

    Make name of the user optional

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
     id: int = 0
-    name: str
+    name: Optional[str]
     email: str
     password: str
